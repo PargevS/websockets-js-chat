302bc5fb46f7c76505c786a22fde379a756a57ec
PargevS <sargsyanpargev1991@gmail.com>
2021-04-04 18:26:25 +0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.scss';
+import ES_Chat from "./chat/ES_Chat";
+
+
+function App() {
+  return (
+    <div className="app">
+      <ES_Chat/>
+    </div>
+  );
+}
+
+export default App;
